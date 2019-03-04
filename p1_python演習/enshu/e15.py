f89fb8e2b36370d43ab18a5f710a265d901a40dd
kaisei <kaise64taka84@keio.jp>
2019-03-04 18:43:29 +0900

    15問目解答追加 更新

--- p1_python演習/enshu/e15.py
+++ p1_python演習/enshu/e15.py
@@ -1,48 +1,27 @@
-from math import log
-import pandas as pd
 import numpy as np
 import math
 
-docs = [["リンゴ", "リンゴ"], ["リンゴ", "レモン"], ["レモン", "ミカン"]]
-terms = ["リンゴ", "レモン", "ミカン"]
-N = len(docs)
 
 def tf(term, doc):
     return doc.count(term) / len(doc)
 
-result = []
-for i in range(N):
-    result.append([])
-    d = docs[i]
-    for j in range(len(terms)):
-        t = terms[j]
 
-        result[-1].append(tf(t, d))
-
-tf_ = pd.DataFrame(result, columns=terms)
-
-
-def idf(term, doc):
+def idf(term, docs):
     df = 0
     for doc in docs:
         df += term in doc
 
-    return math.log10(N / df) + 1
+    return math.log10(len(docs) / df) + 1
 
-result2 = []
-for j in range(len(terms)):
-    t = terms[j]
-    d = docs[j]
-    result2.append(idf(t, d))
 
-idf_ = pd.DataFrame(result2, index=terms, columns=["IDF"])
-
-def tfidf(tf1, idf1):
+def tf_idf(terms, docs):
     x = np.zeros((3, 3))
-    for i in range(3):
-        for j in range(3):
-            x[i][j] = tf1[i][j] * idf1[j]
-
+    for i, doc in enumerate(docs):
+        for j, term in enumerate(terms):
+            x[i][j] = tf(term, doc) * idf(term, docs)
     return x
 
-print(tfidf(result, result2))
+docs = [["リンゴ", "リンゴ"], ["リンゴ", "レモン"], ["レモン", "ミカン"]]
+terms = ["リンゴ", "レモン", "ミカン"]
+
+print(tf_idf(terms, docs))
