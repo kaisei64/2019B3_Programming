34cfabe4fa7695638b783df179a6627ed0dddfd5
kaisei <kaise64taka84@keio.jp>
2019-03-11 11:52:11 +0900

    演習1−2追加

--- p2_ニューラルネットワーク実装/enshu/e1_2.py
+++ p2_ニューラルネットワーク実装/enshu/e1_2.py
@@ -9,10 +9,14 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     OR = Perceptron(0.5, 0.2, 0.1)
 
-x1_list = [1, 1, 0, 0]
-x2_list = [1, 0, 1, 0]
+def main():
+    x1_list = [1, 1, 0, 0]
+    x2_list = [1, 0, 1, 0]
 
-for x1, x2 in zip(x1_list,x2_list):
-    print("AND({0}, {1}) = {2}  ".format(x1, x2, AND.Forward(x1, x2)), end="")
-    print("NAND({0}, {1}) = {2}  ".format(x1, x2, NAND.Forward(x1, x2)), end="")
-    print("OR({0}, {1}) = {2}  ".format(x1, x2, OR.Forward(x1, x2)))
+    for x1, x2 in zip(x1_list,x2_list):
+        print("AND({0}, {1}) = {2}  ".format(x1, x2, AND.Forward(x1, x2)), end="")
+        print("NAND({0}, {1}) = {2}  ".format(x1, x2, NAND.Forward(x1, x2)), end="")
+        print("OR({0}, {1}) = {2}  ".format(x1, x2, OR.Forward(x1, x2)))
+
+if __name__ == "__main__":
+    main()
