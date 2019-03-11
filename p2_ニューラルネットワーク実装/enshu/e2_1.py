420cb285efe80b9d1299bed38db37ae415e544f7
kaisei <kaise64taka84@keio.jp>
2019-03-11 11:53:37 +0900

    演習2−1追加

--- p2_ニューラルネットワーク実装/enshu/e2_1.py
+++ p2_ニューラルネットワーク実装/enshu/e2_1.py
@@ -1,11 +1,15 @@
 import numpy as np
 import math
 
+e = math.e
 
 def Sigmoid(x):
     return 1 / (1 + e**-x)
 
-x = np.array([-1.0, 0.0, 0.5, 2.0])
-e = math.e
+def main():
+    x = np.array([-1.0, 0.0, 0.5, 2.0])
 
-print(Sigmoid(x))
+    print(Sigmoid(x))
+
+if __name__ == "__main__":
+    main()
