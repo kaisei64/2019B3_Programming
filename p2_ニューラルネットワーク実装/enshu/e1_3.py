7932e0ce42eef5b728e4e67c0e3e1757b07a4568
kaisei <kaise64taka84@keio.jp>
2019-03-11 11:29:11 +0900

    演習1−3追加

---
+++ p2_ニューラルネットワーク実装/enshu/e1_3.py
@@ -0,0 +1,25 @@
+from e1_1 import Perceptron
+
+if __name__ == "__main__":
+    AND = Perceptron(1.1, 2.1, 2.2)
+
+if __name__ == "__main__":
+    NAND = Perceptron(-1.1, -2.1, -2.2)
+
+if __name__ == "__main__":
+    OR = Perceptron(0.5, 0.2, 0.1)
+
+def XOR(x1, x2):
+    s1 = Perceptron(-1.1, -2.1, -2.2)
+    s2 = Perceptron(0.5, 0.2, 0.1)
+    s3 = Perceptron(1.1, 2.1, 2.2)
+    t1 = s1.Forward(x1, x2)
+    t2 = s2.Forward(x1, x2)
+    y = s3.Forward(t1, t2)
+    return y
+
+x1_list = [1, 1, 0, 0]
+x2_list = [1, 0, 1, 0]
+
+for x1, x2 in zip(x1_list,x2_list):
+    print("XOR({0}, {1}) = {2}  ".format(x1, x2, XOR(x1, x2)))
