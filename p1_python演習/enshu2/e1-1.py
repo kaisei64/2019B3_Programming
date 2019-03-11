a04ef4cbf3fae4e14b17cb598cdcadecb5b21357
kaisei <kaise64taka84@keio.jp>
2019-03-11 10:44:26 +0900

    演習1−1追加

--- p1_python演習/enshu2/e1-1.py
+++ p1_python演習/enshu2/e1-1.py
@@ -10,20 +10,3 @@ class Perceptron:
             return 1
         elif y < self.theta:
             return 0
-
-if __name__ == "__main__":
-    AND = Perceptron(1.1, 2.1, 2.2)
-
-if __name__ == "__main__":
-    NAND = Perceptron(-1.1, -2.1, -2.2)
-
-if __name__ == "__main__":
-    OR = Perceptron(0.5, 0.2, 0)
-
-x1_list = [1, 1, 0, 0]
-x2_list = [1, 0, 1, 0]
-
-for i,j in x1_list,x2_list:
-    print("AND(%d, %d) = %d",%(x1_list[i], x2_list[j], AND.Forward(x1_list[i], x2_list[j])), end="")
-    print("NAND(%d, %d) = %d", % (x1_list[i], x2_list[j], NAND.Forward(x1_list[i], x2_list[j])), end="")
-    print("OR(%d, %d) = %d", % (x1_list[i], x2_list[j], OR.Forward(x1_list[i], x2_list[j])))
