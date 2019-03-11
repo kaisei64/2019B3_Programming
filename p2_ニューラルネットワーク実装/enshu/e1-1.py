4db5e9f7b4a05606e199c0b5a1207dff787c43bd
kaisei <kaise64taka84@keio.jp>
2019-03-11 10:48:59 +0900

    演習1−1追加

---
+++ p2_ニューラルネットワーク実装/enshu/e1-1.py
@@ -0,0 +1,13 @@
+class Perceptron:
+    def __init__(self, w1, w2, theta):
+        self.w1 = w1
+        self.w2 = w2
+        self.theta = theta
+
+    def Forward(self, x1, x2):
+        y = self.w1 * x1 + self.w2 * x2
+        if y >= self.theta:
+            return 1
+        elif y < self.theta:
+            return 0
+
